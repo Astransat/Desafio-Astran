401868d1876c78ff162a97d52eb92ac04bd1f55b
Marcos-Cypriano <ma_cypriano@hotmail.com>
2022-06-03 13:59:10 -0300

    Set backend structure, add get & history routes

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, Query } from '@nestjs/common'
+import { AppService } from './app.service'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/stocks/:stock_name/quote')
+  async getStockQuote(@Param('stock_name') stock_name: string): Promise<Object> {
+    return await this.appService.getStockQuote(stock_name)
+  }
+
+  @Get('/stocks/:stock_name/history?')
+  async getStockHistory(@Param('stock_name') stock_name: string, @Query('from') from: string, @Query('to') to: string): Promise<Object> {
+    return await this.appService.getStockHistory(stock_name, from, to)
+  }
+}
---
+++ backend/src/app.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@nestjs/common'
+import callExternalApi from './utils'
+
+
+@Injectable()
+export class AppService {
+  async getStockQuote(stock_name: string): Promise<Object> {
+    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock_name}&apikey=${process.env.ALPHAVANTAGE_KEY}`
+
+    const json = await callExternalApi(url).then((data) => {
+      const response = {
+        body: data // pode fazer um JSON.stringfy também
+      }
+      return response
+    })
+
+    //Working with integers so that we don't encounter rounding problems from language's way of storaging floating numbers
+    const price = parseInt(json["body"]["Global Quote"]["05. price"].replace('.',''))
+    const result = {
+      name: json["body"]["Global Quote"]["01. symbol"],
+      lastPrice: price / 10000, //Restoring the number's dimension
+      pricedAt: json["body"]["Global Quote"]["07. latest trading day"]
+    }
+
+    return result
+  }
+
+
+  async getStockHistory(stock_name: string, from: string, to: string): Promise<Object> {
+    // Adding a second on each date so that getDate returns the wright date
+    const startDate = new Date(`${from} 00:00:01`)
+    const endDate = new Date(`${to} 00:00:01`)
+
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock_name}&outputsize=full&apikey=${process.env.ALPHAVANTAGE_KEY}`
+
+
+    const allDates = await callExternalApi(url).then((data) => {
+      const response = {
+        body: data // pode fazer um JSON.stringfy também
+      }
+      return response
+    })
+
+    var prices = []
+    
+    for (var i = startDate; i <= endDate; i.setDate(i.getDate() + 1 )) {
+      // Handling month
+      if (`${i.getMonth()}`.length == 1) {
+        var month = `0${ i.getMonth() + 1 }`
+      } else {
+        var month = `${ i.getMonth() + 1 }`
+      }
+      
+      // Handling day
+      if (`${i.getDate()}`.length == 1) {
+        var day = `0${i.getDate()}`
+      } else {
+        var day = `${i.getDate()}`
+      }
+
+      const date = `${i.getFullYear()}-${month}-${day}`
+
+      // Checking if the date exists in the data so it goes to the prices array
+      if (allDates["body"]["Time Series (Daily)"][date]) {
+        //Working with integers so that we don't encounter rounding problems from language's way of storaging floating numbers
+        const opening = (parseInt(allDates["body"]["Time Series (Daily)"][date]['1. open'].replace('.',''))) / 10000
+        const low = (parseInt(allDates["body"]["Time Series (Daily)"][date]['3. low'].replace('.',''))) / 10000
+        const high = (parseInt(allDates["body"]["Time Series (Daily)"][date]['2. high'].replace('.',''))) / 10000
+        const closing = (parseInt(allDates["body"]["Time Series (Daily)"][date]['4. close'].replace('.',''))) / 10000
+  
+        const price = {
+          opening,
+          low,
+          high,
+          closing,
+          pricedAt: date
+        }
+  
+        prices.push(price)
+      }     
+    }
+
+    const result = {
+      name: stock_name,
+      prices
+    }
+
+    return result
+  }
+}
+
